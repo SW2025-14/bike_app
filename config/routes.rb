842695d841f475ebcc8b2d561c5624271b3591cd
小川泰生 <2311140059g@ed.fuk.kindai.ac.jp>
2025-11-26 08:12:08 +0000

    Set root to posts#index for user_post branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get "posts/index"
-  get "posts/new"
-  get "posts/create"
-  get "posts/show"
+  root "posts/index"
+  resources :posts
+  resources :bikes
+  resources :reviews
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
