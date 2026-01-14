bf126f51d1ea9a68f85b2ac7cc90788bce1d1324
Taisuke Mori <2311140043r@ed.fuk.kindai.ac.jp>
2026-01-14 09:22:20 +0000

    last3

--- app/models/like.rb
+++
@@ -1,5 +0,0 @@
-class Post < ApplicationRecord
-  has_one_attached :photo
-  validates :name, :user_name, :gender, presence: true
-  validates :likes, numericality: { greater_than_or_equal_to: 0 }
-end
